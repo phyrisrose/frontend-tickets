c817fc4bf327f835098d243350bf3ca1af96bad0
Anna <phyrisrose@gmail.com>
2023-06-15 18:59:18 -0600

    penciling in what shows data might look like

---
+++ src/app/shows-data.ts
@@ -0,0 +1,11 @@
+import { Show } from './shows.types';
+
+export const shows: Show[] = [
+  {
+    id: 23,
+    musician: 'Youth Lagoon',
+    date: new Date(2023, 5, 3),
+    location: 'Fort Collins, CO',
+    price: 30,
+  },
+];
---
+++ src/app/shows.types.ts
@@ -0,0 +1,8 @@
+export type Show = {
+  id: number;
+  musician: string;
+  date: Date;
+  location: string;
+  price: number;
+  imageUrl?: string;
+};
