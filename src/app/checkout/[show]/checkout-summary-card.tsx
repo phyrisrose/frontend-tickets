32fd9fb4ab7fbbd64ad692386d5a188917e88e11
Anna <phyrisrose@gmail.com>
2023-06-16 18:58:09 -0600

    adding scaffolding for `CheckoutSummaryCard`

---
+++ src/app/checkout/[show]/checkout-summary-card.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/components';
+
+/**
+ * @note the number of tickets get selected here
+ * and the total due is displayed
+ * (you can add some fake fees)
+ */
+export default function CheckoutSummaryCard() {
+  return <Card className="col-span-1 p-4">Summary</Card>;
+}
--- src/app/checkout/[show]/page.tsx
+++ src/app/checkout/[show]/page.tsx
@@ -1,5 +1,6 @@
 import { shows } from '@/app/shows-data';
 import PaymentCard from './payment-card';
+import CheckoutSummaryCard from './checkout-summary-card';
 
 interface CheckoutProps {
   params: {
@@ -9,13 +10,12 @@ interface CheckoutProps {
 
 export default function Checkout({ params }: CheckoutProps) {
   const show = shows[params.show];
+  // Checkout page for {show.musician} in {show.location}
 
   return (
     <div className="grid gap-6 grid-cols-1 sm:grid-cols-3">
       <PaymentCard />
-      <div className="col-span-1">
-        Checkout page for {show.musician} in {show.location}
-      </div>
+      <CheckoutSummaryCard />
     </div>
   );
 }
