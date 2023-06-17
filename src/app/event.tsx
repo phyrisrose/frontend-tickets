4e1762e86d5a4cc08726145d673ec4f35dc79341
Anna <phyrisrose@gmail.com>
2023-06-16 18:13:28 -0600

    adding hover animations to show list items

--- src/app/event.tsx
+++ src/app/event.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Show } from './shows.types';
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid';
 
 interface EventProps {
   show: Show;
@@ -10,18 +11,39 @@ interface EventProps {
  */
 export default function Event({ show }: EventProps) {
   return (
-    <div className="flex gap-4">
-      <Image
-        className="h-20 w-20 rounded-full object-cover border-2 border-slate-200"
-        src={show.imageUrl}
-        alt="show-avatar"
-        height={40}
-        width={40}
-      />
-      <div>
-        <div>{show.date.getFullYear()}</div>
-        <div>{show.musician}</div>
-        <div>{show.location}</div>
+    <div className="flex gap-12 items-center justify-between">
+      <div className="flex gap-4">
+        <Image
+          className="h-20 w-20 rounded-full object-cover border-2 border-slate-200"
+          src={show.imageUrl}
+          alt="show-avatar"
+          height={40}
+          width={40}
+        />
+        <div>
+          <div>{show.date.getFullYear()}</div>
+          <div>{show.musician}</div>
+          <div>{show.location}</div>
+        </div>
+      </div>
+      <div className="w-200 shrink-0">
+        <ArrowLongRightIcon
+          className={[
+            'fill-current',
+            'text-gray-500',
+            'inline',
+            'invisible',
+            'group-hover/item:visible',
+            'opacity-0',
+            'group-hover/item:opacity-100',
+            'transition-all',
+            '-translate-x-10',
+            'group-hover/item:translate-x-0',
+            'duration-500',
+            'h-7',
+            'ml-2',
+          ].join(' ')}
+        />
       </div>
     </div>
   );
