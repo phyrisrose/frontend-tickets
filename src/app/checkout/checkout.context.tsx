71d8d7bdd399b4358760ce689f43d41140de5349
Anna <phyrisrose@gmail.com>
2023-07-29 18:37:10 -0600

    extracting checkout reducer to its own file

--- src/app/checkout/checkout.context.tsx
+++ src/app/checkout/checkout.context.tsx
@@ -7,51 +7,16 @@ import {
   useContext,
 } from 'react';
 
-import type { PaymentInfo } from './checkout.types';
-
-type NumberOfTicketsAction = { payload: number; type: 'setNumberOfTickets' };
-type UpdatePaymentAction = {
-  payload: Partial<PaymentInfo>;
-  type: 'updatePayment';
-};
-type Action = NumberOfTicketsAction | UpdatePaymentAction;
-type Dispatch = (action: Action) => void;
-type State = { paymentInfo: PaymentInfo; numberOfTickets: number };
-
-const initialState = {
-  paymentInfo: {
-    cardholderName: '',
-    cardNumber: '',
-    expirationMonth: 'MM',
-    expirationYear: 'YYYY',
-    securityCode: '',
-  } as PaymentInfo,
-  numberOfTickets: 2,
-};
+import {
+  type Dispatch,
+  type State,
+  initialState,
+  checkoutReducer,
+} from './checkout.state';
 
 const CheckoutStateContext = createContext<
   { dispatch: Dispatch; state: State } | undefined
 >(undefined);
-
-/** @todo take the reducer out into a different file */
-function checkoutReducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'setNumberOfTickets': {
-      return { ...state, numberOfTickets: action.payload };
-    }
-
-    case 'updatePayment': {
-      return {
-        ...state,
-        paymentInfo: { ...state.paymentInfo, ...action.payload },
-      };
-    }
-
-    default: {
-      throw new Error(`Unhandled action type: ${(action as Action).type}`);
-    }
-  }
-}
 
 function useCheckoutContext() {
   const context = useContext(CheckoutStateContext);
