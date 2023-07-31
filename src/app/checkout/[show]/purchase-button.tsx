17648ea3a4d4c1eccadc8b869ffa8b1f9b950cca
Anna <phyrisrose@gmail.com>
2023-07-30 20:36:58 -0600

    `@todo` for post-submission

--- src/app/checkout/[show]/purchase-button.tsx
+++ src/app/checkout/[show]/purchase-button.tsx
@@ -6,6 +6,10 @@ import { purchase } from '../checkout.api';
 export default function PurchaseButton() {
   const { state } = useCheckoutContext();
 
+  /**
+   * @todo from here, we'd probably want to redirect
+   * to some sort of confirmation / order summary page
+   */
   const handlePurchase = async () => {
     await purchase(state);
   };
