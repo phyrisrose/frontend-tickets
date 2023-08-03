14f81aefb3a375b04f4704efe3f45eba52a9549b
Anna <phyrisrose@gmail.com>
2023-08-02 18:20:50 -0600

    cleaning up the purchase submission endpoint

--- src/app/checkout/checkout.api.ts
+++ src/app/checkout/checkout.api.ts
@@ -1,6 +1,20 @@
 import { postData } from '@/lib';
+import { PaymentInfo } from './checkout.types';
 
-/** @ts-expect-error @todo add a type for data */
-export const purchase = (data) => {
-  return postData('https://jsonplaceholder.typicode.com/posts', data);
+interface PurchaseData {
+  paymentInfo: PaymentInfo;
+  numberOfTickets: number;
+}
+
+/**
+ * This is a fake submission endpoint for concert info
+ *
+ * @param data {PurchaseData}
+ * @returns the data submitted is echoed, with an id
+ */
+export const purchase = ({ numberOfTickets, paymentInfo }: PurchaseData) => {
+  return postData('https://jsonplaceholder.typicode.com/posts', {
+    numberOfTickets,
+    paymentInfo,
+  });
 };
